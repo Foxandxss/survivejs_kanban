13d1d7f13210e36a45d9e4ad3f085efd1537a170
Jesus Rodriguez <foxandxss@gmail.com>
2015-08-04 21:41:33 +0200

    Be able to edit items for real

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Notes from './Notes';
 import uuid from 'node-uuid';
+import findIndex from 'find-index';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -48,6 +49,15 @@ export default class App extends React.Component {
   }
 
   itemEdited(noteId, task) {
-    console.log('item edited', noteId, task);
+    let notes = this.state.notes;
+    const noteIndex = findIndex(notes, (o) => o.id === noteId);
+
+    if (noteIndex < 0) {
+      return console.warn('Failed to find the note', notes, noteId);
+    }
+
+    notes[noteIndex].task = task;
+
+    this.setState({notes});
   }
 }
