eb545558e5a2291cebbbedcdaadf2fa57dcd3b0f
Jesus Rodriguez <foxandxss@gmail.com>
2015-08-04 20:18:02 +0200

    Dummy add item, yay

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -29,8 +29,13 @@ export default class App extends React.Component {
 
     return (
       <div>
+        <button onClick={this.addItem}>+</button>
         <Notes items={notes} />
       </div>
     );
+  }
+
+  addItem() {
+    console.log('add item');
   }
 }
