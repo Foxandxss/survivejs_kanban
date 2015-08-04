4d8352e8a65cf10b49f47e4d65ce05e4ffc145db
Jesus Rodriguez <foxandxss@gmail.com>
2015-08-04 11:50:57 +0200

    List of dummy notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
 import Note from './Note';
+import uuid from 'node-uuid';
 
 export default class App extends React.Component {
   render() {
-    return <Note />;
+    const notes = [
+      {
+        id: uuid.v4(),
+        task: 'Learn webpack'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Learn React'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Do Laundry'
+      }
+    ];
+
+    return (
+      <div>
+        <ul>{notes.map(this.renderNote)}</ul>
+      </div>
+    );
+  }
+  renderNote(note) {
+    return (
+      <li key={`note${note.id}`}>
+        <Note value={note.task} />
+      </li>
+    );
   }
 }
