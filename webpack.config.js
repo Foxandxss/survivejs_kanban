8c27c97223c588d129557c4398ba970583bef58b
Jesus Rodriguez <foxandxss@gmail.com>
2015-09-03 16:49:39 +0200

    Extract hashed css

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ var HtmlwebpackPlugin = require('html-webpack-plugin');
 var merge = require('webpack-merge');
 var pkg = require('./package.json');
 var Clean = require('clean-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
@@ -14,15 +15,6 @@ var common = {
     path: path.resolve(ROOT_PATH, 'build'),
     filename: 'bundle.js'
   },
-  module: {
-    loaders: [
-      {
-        test: /\.css$/,
-        loaders: ['style', 'css'],
-        include: path.resolve(ROOT_PATH, 'app')
-      }
-    ]
-  },
   plugins: [
     new HtmlwebpackPlugin({
       title: 'Kanban app'
@@ -30,11 +22,16 @@ var common = {
   ]
 };
 
-if (TARGET === 'start') {
+if (TARGET === 'start' || !TARGET) {
   module.exports = merge(common, {
     devtool: 'eval',
     module: {
       loaders: [
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css'],
+          include: path.resolve(ROOT_PATH, 'app')
+        },
         {
           test: /\.jsx?$/,
           loaders: ['react-hot', 'babel'],
@@ -69,6 +66,11 @@ if (TARGET === 'build') {
     module: {
       loaders: [
         {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('style', 'css'),
+          include: path.resolve(ROOT_PATH, 'app')
+        },
+        {
           test: /\.jsx?$/,
           loaders: ['babel'],
           include: path.resolve(ROOT_PATH, 'app')
@@ -77,6 +79,7 @@ if (TARGET === 'build') {
     },
     plugins: [
       new Clean(['build']),
+      new ExtractTextPlugin('styles.[chunkhash].css'),
       new webpack.optimize.CommonsChunkPlugin(
         'vendor',
         '[name].[chunkhash].js'
